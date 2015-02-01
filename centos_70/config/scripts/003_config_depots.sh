452227d474289e1b9e6ba72368f1dfd8b7bb2783
arllinux <jeanpierre.antinoux@free.fr>
2015-02-02 00:01:38 +0100

    Modif paramètre réseau

--- centos_70/config/scripts/003_config_depots.sh
+++ centos_70/config/scripts/003_config_depots.sh
@@ -13,9 +13,13 @@ echo "----------------------------------------------------------"
    cat $CWD/../repositories/CentOS-Base.repo >\
    /etc/yum.repos.d/CentOS-Base.repo
 
-# Réglage du dépot rpmforge
+# Réglage du dépot rpmforge-release
    cat $CWD/../repositories/rpmforge.repo >\
    /etc/yum.repos.d/rpmforge.repo
+
+# Réglage du dépot epel-release
+   cat $CWD/../repositories/epel.repo >\
+   /etc/yum.repos.d/epel.repo
 
 # Nettoyage, mise à jour et vérification dépots
    yum repolist
