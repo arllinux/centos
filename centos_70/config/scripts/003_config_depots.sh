d608ee0348c3869c1baf74f8bd6884b64b08cd40
arllinux <jeanpierre.antinoux@free.fr>
2015-05-02 18:28:59 +0200

    ...

--- centos_70/config/scripts/003_config_depots.sh
+++ centos_70/config/scripts/003_config_depots.sh
@@ -18,13 +18,16 @@ else
   	 cat $CWD/../repositories/CentOS-Base.repo >\
    	/etc/yum.repos.d/CentOS-Base.repo
 
+	# Téléchargement du paquet rpmforge
+	#	lynx http://apt.sw.be/redhat/el7/en/x86_64/rpmforge/RPMS/
+
 	# Réglage du dépot rpmforge-release
-  	 cat $CWD/../repositories/rpmforge.repo >\
-   	/etc/yum.repos.d/rpmforge.repo
+  #	cat $CWD/../repositories/rpmforge.repo >\
+  #	/etc/yum.repos.d/rpmforge.repo
 
 	# Réglage du dépot epel-release
-  	 cat $CWD/../repositories/epel.repo >\
-   	/etc/yum.repos.d/epel.repo
+  #	 cat $CWD/../repositories/epel.repo >\
+  #	/etc/yum.repos.d/epel.repo
 
 	# Nettoyage, mise à jour et vérification dépots
 		yum repolist
